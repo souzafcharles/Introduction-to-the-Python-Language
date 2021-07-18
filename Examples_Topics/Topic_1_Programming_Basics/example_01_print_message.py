4695259c763bf325c41c3de6189ce41b09661060
Charles Fernandes de Souza <souzach@outlook.com>
2021-07-18 17:13:05 -0300

    create examples in Python

---
+++ Examples_Topics/Topic_1_Programming_Basics/example_01_print_message.py
@@ -0,0 +1,9 @@
+# Course title: Udemy Course: Introduction to the Python Language
+# Instructor: Prof. Dr. Diego Mariano
+# Example adapted by: Charles Fernandes de Souza
+# Date: July 10, 2021
+
+# Print Message
+
+print("Hello World!")
+print("Running Google Colab with Python")
