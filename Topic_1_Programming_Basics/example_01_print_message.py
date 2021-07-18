77650d1e3abc48c4fbc87ec5cd11ff35d800e7ed
Charles Fernandes de Souza <souzach@outlook.com>
2021-07-18 17:24:50 -0300

    Delete Topic_1_Programming_Basics directory

--- Topic_1_Programming_Basics/example_01_print_message.py
+++
@@ -1,9 +0,0 @@
-# Course title: Udemy Course: Introduction to the Python Language
-# Instructor: Prof. Dr. Diego Mariano
-# Example adapted by: Charles Fernandes de Souza
-# Date: July 10, 2021
-
-# Print Message
-
-print("Hello World!")
-print("Running Google Colab with Python")
